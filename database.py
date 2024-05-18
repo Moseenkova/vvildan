451a3658e814b4d8c581d722666a798d78f78748
Moseenkova <fatimka66666@gmail.com>
2024-05-18 13:04:54 +0500

    requests table

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
+from datetime import datetime
 from os import getenv
 
 from dotenv import load_dotenv
-from sqlalchemy import BigInteger, ForeignKey
+from sqlalchemy import BigInteger, Date, ForeignKey
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker
 
@@ -15,7 +16,7 @@ async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit
 
 
 class Base(DeclarativeBase):
-    pass
+    id: Mapped[int] = mapped_column(primary_key=True)
 
 
 # created_at
@@ -23,17 +24,32 @@ class Base(DeclarativeBase):
 
 class User(Base):
     __tablename__ = "users"
-    tg_id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
+    tg_id: Mapped[int] = mapped_column(
+        BigInteger, primary_key=True
+    )  # unik value,в таблице юзер не должен повтараться
     name: Mapped[str]
 
 
 class Courier(Base):
     __tablename__ = "couriers"
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.tg_id"), primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.tg_id"))
     phone: Mapped[int]
 
 
 class Sender(Base):
     __tablename__ = "senders"
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.tg_id"), primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.tg_id"))
     phone: Mapped[int]
+
+
+class Request(Base):
+    __tablename__ = "requests"
+    sender_id: Mapped[int] = mapped_column(ForeignKey("senders.id"))
+    from_: Mapped[str]
+    to: Mapped[str]
+    date_from: Mapped[datetime] = mapped_column(Date)
+    date_to: Mapped[datetime] = mapped_column(Date)
+    courier_id: Mapped[int] = mapped_column(ForeignKey("courier_id"))
+
+
+Base.metadata.create_all(engine)
