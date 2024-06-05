4f90bfded78f90c743aa8b6f7e149685e8860016
bikmetle <hamid-nv@vivaldi.net>
2024-06-05 10:41:47 +0300

    days keyboard

--- bot.py
+++ bot.py
@@ -13,8 +13,17 @@ from dotenv import load_dotenv
 from sqlalchemy import insert, select
 from sqlalchemy.exc import IntegrityError
 
-from database import Courier, Sender, User, async_session_maker
-from my_keyboards import MyCallback, role_markup
+from database import Country, Courier, Sender, User, async_session_maker
+from my_keyboards import (
+    CountryCallback,
+    DateCallback,
+    MyCallback,
+    country_keyboard,
+    day_keyboard,
+    month_keyboard,
+    months,
+    role_markup,
+)
 
 load_dotenv()
 TOKEN = getenv("BOT_TOKEN")
@@ -43,7 +52,10 @@ async def command_start_handler(message: Message) -> None:
 async def sender_button_handler(
     callback_query: CallbackQuery, callback_data: MyCallback
 ):
-    await callback_query.message.answer("Выберите город отпавления")
+    await callback_query.message.answer(
+        "Отправить из:", reply_markup=await country_keyboard(direction="from")
+    )
+    await callback_query.message.delete()
     async with async_session_maker() as session:
         query = select(User.__table__.columns).filter_by(
             tg_id=callback_query.message.chat.id
@@ -58,7 +70,66 @@ async def sender_button_handler(
             await session.rollback()
             print("sender already exists")
 
+    # await callback_query.answer()
+
+
+@dp.callback_query(MyCallback.filter(F.text == "absent_country"))
+async def absent_country_button_handler(
+    callback_query: CallbackQuery, callback_data: MyCallback
+):
+    await callback_query.message.answer("Свайп на лево и введите название страны")
     await callback_query.answer()
+
+
+@dp.callback_query(CountryCallback.filter(F.text == "country_from"))
+async def country_from_button_handler(
+    callback_query: CallbackQuery, callback_data: CountryCallback
+):
+    await callback_query.message.edit_text(f"Отправить из: {callback_data.name}")
+    await callback_query.message.answer(
+        "Отправить в:", reply_markup=await country_keyboard(direction="to")
+    )
+
+
+@dp.callback_query(CountryCallback.filter(F.text == "country_to"))
+async def country_to_button_handler(
+    callback_query: CallbackQuery, callback_data: CountryCallback
+):
+    await callback_query.message.edit_text(f"Отправить в: {callback_data.name}")
+    await callback_query.message.answer(
+        "Месяц:", reply_markup=await month_keyboard(date="from")
+    )
+
+
+@dp.callback_query(DateCallback.filter(F.text == "month_from"))
+async def country_button_handler(
+    callback_query: CallbackQuery, callback_data: DateCallback
+):
+    await callback_query.message.edit_text(f"Дата: {months[callback_data.month]}")
+    await callback_query.message.answer(
+        "День:", reply_markup=await day_keyboard(date="from", month=callback_data.month)
+    )
+
+
+@dp.message()
+async def echo_handler(message: Message) -> None:
+    if not message.reply_to_message:
+        return
+
+    if message.reply_to_message.text == "Свайп на лево и введите название страны":
+        async with async_session_maker() as session:
+            query = select(User.__table__.columns).filter_by(tg_id=message.chat.id)
+            result = await session.execute(query)
+            user = result.mappings().one_or_none()
+            try:
+                query = insert(Country).values(
+                    name=message.text, created_by_id=user["id"]
+                )
+                await session.execute(query)
+                await session.commit()
+            except IntegrityError:
+                await session.rollback()
+                print("Country already exists")
 
 
 @dp.callback_query(MyCallback.filter(F.text == "courier"))
--- my_keyboards.py
+++ my_keyboards.py
@@ -1,6 +1,27 @@
+from datetime import datetime, timedelta
+
 from aiogram.filters.callback_data import CallbackData
 from aiogram.types.inline_keyboard_button import InlineKeyboardButton
 from aiogram.types.inline_keyboard_markup import InlineKeyboardMarkup
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from sqlalchemy import select
+
+from database import Country, async_session_maker
+
+months = {
+    1: "Январь",
+    2: "Февраль",
+    3: "Март",
+    4: "Апрель",
+    5: "Май",
+    6: "Июнь",
+    7: "Июль",
+    8: "Август",
+    9: "Сентябрь",
+    10: "Октябрь",
+    11: "Ноябрь",
+    12: "Декабрь",
+}
 
 
 class MyCallback(CallbackData, prefix="my"):
@@ -16,3 +37,82 @@ courier_button = InlineKeyboardButton(
 )
 
 role_markup = InlineKeyboardMarkup(inline_keyboard=[[sender_button, courier_button]])
+
+
+class CountryCallback(CallbackData, prefix="country"):
+    text: str
+    name: str
+
+
+async def country_keyboard(direction):
+    builder = InlineKeyboardBuilder()
+    async with async_session_maker() as session:
+        query = select(Country.__table__.columns)
+        result = await session.execute(query)
+        countries = result.mappings().all()
+
+    for country in countries:
+        builder.button(
+            text=country.name,
+            callback_data=CountryCallback(
+                text=f"country_{direction}", name=country.name
+            ).pack(),
+        )
+    builder.button(
+        text="Нет в списке", callback_data=MyCallback(text="absent_country").pack()
+    )
+
+    return builder.as_markup()
+
+
+class DateCallback(CallbackData, prefix="date"):
+    text: str
+    month: int = 0
+    day: int = 0
+
+
+async def month_keyboard(date):
+    builder = InlineKeyboardBuilder()
+    today = datetime.today().replace(day=15)
+    current_month = today
+    next_month = today + timedelta(days=30)
+    after_next_month = today + timedelta(days=60)
+    builder.button(
+        text=months[current_month.month],
+        callback_data=DateCallback(
+            text=f"month_{date}", month=current_month.month
+        ).pack(),
+    )
+    builder.button(
+        text=months[next_month.month],
+        callback_data=DateCallback(text=f"month_{date}", month=next_month.month).pack(),
+    )
+    builder.button(
+        text=months[after_next_month.month],
+        callback_data=DateCallback(
+            text=f"month_{date}", month=after_next_month.month
+        ).pack(),
+    )
+
+    return builder.as_markup()
+
+
+async def day_keyboard(date, month):
+    builder = InlineKeyboardBuilder()
+    today = datetime.today()
+    if today.month == month:
+        first_day_of_month = today
+    else:
+        first_day_of_month = today.replace(month=month, day=1)
+    days_in_month = []
+    current_day = first_day_of_month
+    while current_day.month == month:
+        days_in_month.append(current_day.day)
+        current_day += timedelta(days=1)
+    for day in days_in_month:
+        builder.button(
+            text=str(day),
+            callback_data=DateCallback(text=f"day_{date}", day=day).pack(),
+        )
+
+    return builder.as_markup()
