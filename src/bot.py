e6efb3abdd27e28ca4191c6c2818aeabb7373d92
Moseenkova <fatimka66666@gmail.com>
2024-07-10 11:00:57 +0500

    from_router

--- src/bot.py
+++ src/bot.py
@@ -3,9 +3,11 @@ import logging
 import sys
 from os import getenv
 
-from aiogram import Bot, Dispatcher, F
+from aiogram import Bot, Dispatcher, F, Router
+from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
+from aiogram.fsm.state import State, StatesGroup
 from aiogram.types import CallbackQuery, Message
 from aiogram.utils.markdown import hbold
 from dotenv import load_dotenv
@@ -16,11 +18,24 @@ from my_keyboards import GeneralCallback, RoleCallback, country_keyboard, role_m
 # Load environment variables from a .env file
 load_dotenv()
 TOKEN = getenv("BOT_TOKEN")
-dp = Dispatcher()
+bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+form_router = Router()
+
+
+class Form(StatesGroup):
+    name = State()
+    role = State()
+    city_from = State()
+    city_to = State()
+    month_from = State()
+    month_to = State()
+    day_from = State()
+    day_to = State()
+    message_id = State()
 
 
 # Handler for the /start command
-@dp.message(CommandStart())
+@form_router.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
     async with async_session_maker() as session:
         await get_or_create(
@@ -36,7 +51,7 @@ async def command_start_handler(message: Message) -> None:
 
 
 # Handler for the 'sender' role callback
-@dp.callback_query(RoleCallback.filter(F.text == "sender"))
+@form_router.callback_query(RoleCallback.filter(F.text == "sender"))
 async def sender_button_handler(
     callback_query: CallbackQuery, callback_data: RoleCallback
 ):
@@ -55,7 +70,7 @@ async def sender_button_handler(
 
 
 # Handler for the 'courier' role callback
-@dp.callback_query(RoleCallback.filter(F.text == "courier"))
+@form_router.callback_query(RoleCallback.filter(F.text == "courier"))
 async def courier_button_handler(
     callback_query: CallbackQuery, callback_data: RoleCallback
 ):
@@ -73,7 +88,7 @@ async def courier_button_handler(
         await get_or_create(session, Courier, user_id=user.id)
 
 
-@dp.callback_query(GeneralCallback.filter(F.text == "absent_country_from"))
+@form_router.callback_query(GeneralCallback.filter(F.text == "absent_country_from"))
 async def absent_country_from_button_handler(
     callback_query: CallbackQuery, callback_data: GeneralCallback
 ):
@@ -84,7 +99,7 @@ async def absent_country_from_button_handler(
     await callback_query.answer()
 
 
-@dp.callback_query(GeneralCallback.filter(F.text == "absent_country_to"))
+@form_router.callback_query(GeneralCallback.filter(F.text == "absent_country_to"))
 async def absent_country_to_button_handler(
     callback_query: CallbackQuery, callback_data: GeneralCallback
 ):
@@ -96,7 +111,7 @@ async def absent_country_to_button_handler(
 
 
 # Handler for processing text input after the "not in the list" callback
-@dp.message()
+@form_router.message()
 async def text_input_handler(message: Message) -> None:
     if not message.reply_to_message:
         answer = await message.answer("Сделайте свайп по сообщению выше ^^^")
@@ -148,7 +163,9 @@ async def text_input_handler(message: Message) -> None:
 
 # Main function to start the bot
 async def main() -> None:
-    bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
+    dp = Dispatcher()
+    dp.include_router(form_router)
+    # Start event dispatching
     await dp.start_polling(bot)
 
 
