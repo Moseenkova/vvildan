67be07f1b53338efe9e7437088a5edf2fd1606ad
Moseenkova <fatimka66666@gmail.com>
2024-07-10 11:58:35 +0500

    from

--- src/bot.py
+++ src/bot.py
@@ -7,12 +7,14 @@ from aiogram import Bot, Dispatcher, F, Router
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
+from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.types import CallbackQuery, Message
 from aiogram.utils.markdown import hbold
 from dotenv import load_dotenv
 
-from database import Country, Courier, Sender, User, async_session_maker, get_or_create
+import database
+from database import Country, User, async_session_maker, get_or_create
 from my_keyboards import GeneralCallback, RoleCallback, country_keyboard, role_markup
 
 # Load environment variables from a .env file
@@ -50,34 +52,18 @@ async def command_start_handler(message: Message) -> None:
     )
 
 
-# Handler for the 'sender' role callback
-@form_router.callback_query(RoleCallback.filter(F.text == "sender"))
-async def sender_button_handler(
-    callback_query: CallbackQuery, callback_data: RoleCallback
-):
-    await callback_query.message.answer(
-        "Отправить из:", reply_markup=await country_keyboard(direction="from")
-    )
-    await callback_query.message.delete()
-    async with async_session_maker() as session:
-        user, _ = await get_or_create(
-            session,
-            User,
-            defaults={"name": callback_query.message.chat.full_name},
-            tg_id=callback_query.message.chat.id,
-        )
-        await get_or_create(session, Sender, user_id=user.id)
-
-
 # Handler for the 'courier' role callback
-@form_router.callback_query(RoleCallback.filter(F.text == "courier"))
-async def courier_button_handler(
-    callback_query: CallbackQuery, callback_data: RoleCallback
+@form_router.callback_query(RoleCallback.filter())
+async def role_button_handler(
+    callback_query: CallbackQuery, callback_data: RoleCallback, state: FSMContext
 ):
-    await callback_query.message.answer(
-        "Отправить из:", reply_markup=await country_keyboard(direction="from")
+    await state.set_state(Form.city_from)
+    answer = await callback_query.message.answer(
+        "Отправить из:\n(введите название города)"
     )
+    await state.set_data({"role": callback_data.model, "message_id": answer.message_id})
     await callback_query.message.delete()
+    model = getattr(database, callback_data.model)
     async with async_session_maker() as session:
         user, _ = await get_or_create(
             session,
@@ -85,10 +71,23 @@ async def courier_button_handler(
             defaults={"name": callback_query.message.chat.full_name},
             tg_id=callback_query.message.chat.id,
         )
-        await get_or_create(session, Courier, user_id=user.id)
+        await get_or_create(session, model, user_id=user.id)
 
 
-@form_router.callback_query(GeneralCallback.filter(F.text == "absent_country_from"))
+@form_router.message(Form.city_from)
+async def process_city_from(message: Message, state: FSMContext) -> None:
+    await state.update_data(city_from=message.text)
+    text = f"Отправить\nИз: {message.text}"
+    data = await state.get_data()
+    await bot.edit_message_text(
+        text=text, chat_id=message.chat.id, message_id=data["message_id"]
+    )
+    await message.delete()
+    await state.set_state(Form.city_to)
+    await message.answer("Отправить в:\n(введите название города)")
+
+
+@form_router.callback_query(GeneralCallback.filter(F.model == "absent_country_from"))
 async def absent_country_from_button_handler(
     callback_query: CallbackQuery, callback_data: GeneralCallback
 ):
@@ -99,7 +98,7 @@ async def absent_country_from_button_handler(
     await callback_query.answer()
 
 
-@form_router.callback_query(GeneralCallback.filter(F.text == "absent_country_to"))
+@form_router.callback_query(GeneralCallback.filter(F.model == "absent_country_to"))
 async def absent_country_to_button_handler(
     callback_query: CallbackQuery, callback_data: GeneralCallback
 ):
