18671a191ba55141e1cedf7f38e18604eca39a08
Marsel Gaifulin <bikmetle@proton.me>
2026-01-22 10:26:11 +0300

    city-or-country-is-viewed-migration

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
+from sqlalchemy import select
+from database import async_session_maker, Country
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/countries")
+async def get_countries():
+    async with async_session_maker() as session:
+        countries = await session.execute(select(Country))
+        countries = countries.scalars().all()
+        return countries
