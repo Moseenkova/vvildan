b6c66113a57821b11fad5484db61d0da085a2e45
Marsel Gaifulin <bikmetle@proton.me>
2026-02-01 21:49:42 +0300

    get-cities

--- src/schemas.py
+++ src/schemas.py
@@ -7,3 +7,11 @@ class CountrySchema(BaseModel):
 
     class Config:
         from_attributes = True
+
+
+class CitySchema(BaseModel):
+    id: int
+    name: str
+
+    class Config:
+        from_attributes = True
