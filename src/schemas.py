8da114f183ca51822f30487563a5b7e70e1b2a3a
Marsel Gaifulin <bikmetle@proton.me>
2026-08-16 18:39:08 +0700

    courier-one-sender-multiple

--- src/schemas.py
+++ src/schemas.py
@@ -1,32 +1,6 @@
 from pydantic import BaseModel
 
 
-class CountrySchema(BaseModel):
-    id: int
-    name: str
-
-    class Config:
-        from_attributes = True
-
-
-class CitySchema(BaseModel):
-    id: int
-    name: str
-
-    class Config:
-        from_attributes = True
-
-
-class AirportSchema(BaseModel):
-    id: int
-    name: str
-    iata_code: str | None
-    icao_code: str | None
-
-    class Config:
-        from_attributes = True
-
-
 class AirportSearchResultSchema(BaseModel):
     id: int
     name: str
